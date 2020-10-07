4086bf2696a3a2018f2fda3608f65f8e2cea5bc6
Olha Afanasieva <afanasievaolja@gmail.com>
2020-10-07 21:17:28 +0200

    setup basic routes, created dummy pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import Map from './pages/Map';
+import Time from './pages/Time';
+import About from './pages/About';
+import Home from './pages/Home';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/map">Physical space</Link>
+            </li>
+            <li>
+              <Link to="/time">Time</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/map">
+            <Map />
+          </Route>
+          <Route path="/time">
+            <Time />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/pages/Time.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+ 
+function Time() {
+ 
+  return <h1>Time</h1>;
+}
+ 
+export default Time;
